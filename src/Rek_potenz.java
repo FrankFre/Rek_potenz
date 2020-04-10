72c6260469086d7f048484f4b2a5d4333d42d36e
Frank Freisberg <f.freisberg@telekom.de>
2020-04-10 09:48:13 +0200

    nochmal final

--- src/Rek_potenz.java
+++ src/Rek_potenz.java
@@ -5,19 +5,19 @@ public class Rek_potenz {
 
 	public static void main(String[] args) {
 
-		int x = 3;
-		int y = 4;
+		int x = 4;
+		int y = 5;
 
-		System.out.println("Ergebnis: " + potenz(x, y));
+		System.out.println("Ergebnis: " +  x + " hoch " + y + ' ' + potenz(x, y));
 
 	}
-
+ 
 	private static int potenz(int x, int y) {
 
 		erg = erg * x;
 		y--;
 		if (y > 0) {
-			potenz(x, y);
+			potenz(x, y); //Rekursion
 		}
 		return erg;
 	}
